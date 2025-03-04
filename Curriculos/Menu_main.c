aa1bdc89a2600e632574b8a04ddaef924c50d440
Datsc-21 <dannyscholze21@gmail.com>
2025-03-03 20:36:48 -0400

    Atualizações

---
+++ Curriculos/Menu_main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+int opcao;
+
+do{  
+
+printf("================================================= \n");
+printf("|                                               |\n");
+printf("|              BEM VINDO A CRIACAO              |\n");
+printf("|                      DE                       |\n");
+printf("|                  CURRICULOS                   |\n");
+printf("|                                               |\n");
+printf("|            1 - ENTRAR NO SISTEMA              |\n");
+printf("|            0 - SAIR DO SISTEMA                |\n");
+printf("|                                               |\n");
+printf("|                                               |\n");
+printf("================================================= \n");
+scanf("%d", &opcao);
+
+switch(opcao)
+{
+case 1:
+    break;
+case 0:
+    break;
+default:
+printf("Opcao Invalido!!!\n");
+    break;
+}
+ }while (opcao != 0);
+ 
+
+return 0;
+
+}
